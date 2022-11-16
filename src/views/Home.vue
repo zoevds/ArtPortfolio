51744b80fb6f27534bfb14e2a91862e180f47101
unknown <zoevanderschyff13@gmail.com>
2022-11-16 12:34:41 +0200

    changes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,23 +1,19 @@
 <template>
   <div class="home">
-    <img is="artportfolio-logo" alt="Vue logo" src="../assets/Zoe's art portfolio.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img id="artportfolio-logo" alt="Vue logo" src="../assets/Zoe's art portfolio.png">
+   
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import Main from '@/components/Main.vue'
+
 
 export default {
-  name: 'Home',
-  components: {
-    Main
-  }
+ 
 }
 </script>
 <styles>
 #artportfolio-logo{
-  
+  width:25rem;
 }
 </styles>
