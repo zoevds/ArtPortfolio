e8565808f24555358e1a9107f90af83103021b21
unknown <zoevanderschyff13@gmail.com>
2022-11-16 12:28:47 +0200

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <img is="artportfolio-logo" alt="Vue logo" src="../assets/Zoe's art portfolio.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Main from '@/components/Main.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Main
+  }
+}
+</script>
+<styles>
+#artportfolio-logo{
+  
+}
+</styles>
